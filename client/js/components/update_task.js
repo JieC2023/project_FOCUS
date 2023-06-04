58c0f376fc39168a184d3d78a298914a8aebca7e
Adamwo11 <adamwo@adam.com.au>
2023-06-04 22:45:51 +0930

    update almost there error msg in api link

--- client/js/components/update_task.js
+++ client/js/components/update_task.js
@@ -1,27 +1,65 @@
 function renderUpdateTask() {
-	document.querySelector('#page').innerHTML = `
-		<section class='update-task'>
-			<form action="" onSubmit="updateTask(event)">
-				<h2>Update Task</h2>
-				<fieldset>
-					<label for="">Task: </label>
-					<input type="text" name="taskName">
-				</fieldset>
-				<fieldset>
-					<label for="">Description: </label>
-					<input type="text" name="description">
-				</fieldset>
-				<fieldset>
-					<label for="">Priority: </label>
-					<input type="text" name="priorityLevel">
-				</fieldset>
-				<button>Update Task</button>
-			</form>
-		</section>
-	`
-}
 
-// function updateTask(event) {
-// 	const updateBtn =event.target
-// 	const 
+
+	document.querySelector('#page').innerHTML = `
+	<section class='update-task'>
+    <form action="" onSubmit="updateTask(event, ${task_Id})">
+        <h2>Update Task</h2>
+        <fieldset>
+            <label for="">Task: </label>
+            <input type="text" name="taskName">
+        </fieldset>
+        <fieldset>
+            <label for="">Description: </label>
+            <input type="text" name="description">
+        </fieldset>
+        <fieldset>
+            <label for="">Priority: </label>
+            <input type="text" name="priorityLevel">
+        </fieldset>
+        <button>Update Task</button>
+    </form>
+</section>
+`
+}
+// function updateTask(event, taskID) {
+//     event.preventDefault();
+//     const form = event.target;
+//     const data = Object.fromEntries(new FormData(form));
+
+//     fetch(`/api/tasks/update/${taskID}`, {
+//         method: 'PUT',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(data)
+//     })
+//         .then(res => res.json())
+//         .then(updatedTask => {
+//             const index = state.tasks.findIndex(task => task.task_id === taskID);
+//             state.tasks[index] = updatedTask;
+
+//             renderList();
+//         });
 // }
+function updateTask(event) {
+	const updateBtn = event.target;
+	const taskDOM = updateBtn.closest('.task');
+	const taskId = taskDOM.dataset.id;
+	const updatedData = {
+	  taskName: 'Updated Task Name',
+	  description: 'Updated Description',
+	  priorityLevel: 'Updated Priority Level'
+	};
+	fetch(`/api/tasks/update/${taskId}`, {
+	  method: 'PUT',
+	  headers: { 'Content-Type': 'application/json' },
+	  body: JSON.stringify(updatedData)
+	})
+	  .then(res => res.json())
+	  .then(updatedTask => {
+
+		const index = state.tasks.findIndex(t => t.task_id === taskId);
+		// Update the task object with the updated values
+		state.tasks[index] = updatedTask;
+		renderList();
+	  });
+  }
