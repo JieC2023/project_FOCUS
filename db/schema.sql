c8d6f58c7b3e840d6fd91df09cacc283013e02d2
kaipeps <128950149+kaipeps@users.noreply.github.com>
2023-06-05 13:27:14 +0930

    Update schema.sql
    minor indentation fixes, moved DROP command above CREATE commands & added comments for flow

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,18 @@
+-- To create database:
 CREATE DATABASE adhd_db;
 \c adhd_db
 
+-- To reset tables run this command:
+DROP TABLE users, lists, tasks;
+-- Then create as below.
+
+-- To create tables, just run this:
 CREATE TABLE users(
     userId SERIAL PRIMARY KEY,
     name TEXT,
     email TEXT,
     passwordDigest TEXT
 );
-DROP TABLE users;
-SELECT* FROM users;
 
 CREATE TABLE lists(
     listId SERIAL PRIMARY KEY,
@@ -16,17 +20,13 @@ CREATE TABLE lists(
     name TEXT,
     description TEXT
 );
-DROP TABLE lists;
-SELECT* FROM lists;
 
 CREATE TABLE tasks(
     taskId SERIAL PRIMARY KEY,
-	listId INT,
+    listId INT,
     taskName TEXT,
     description TEXT,
-	dueDate DATE,
-	priorityLevel INTEGER,
-	status text
+    dueDate DATE,
+    priorityLevel INTEGER,
+    status text
 );
-DROP TABLE tasks;
-SELECT* FROM tasks;
