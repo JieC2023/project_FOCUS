e6c28f7fe24893592127f47e84f1d8090428fce4
kaipeps <kaipepper99@gmail.com>
2023-06-06 09:33:56 +0930

    My Lists link returned to account page, minor usability updates to task gen

--- client/js/components/account.js
+++ client/js/components/account.js
@@ -1,7 +1,7 @@
 function renderAccount() {
     document.querySelector('#page').innerHTML = `
         <h2>Hey, ${state.loggedInUser.name}!</h2>
-        ${renderUserLists()}
+        <h3 class="link" onClick="renderUserLists()">My Lists</h3>
         <h3 class="link" onClick="renderAddTask()">Start new list</h3>
     `
 }
